14e6ed5dc4d1e814459eed6d30854db49941ad8b
daniel <pejqueen7@gmail.com>
2024-10-02 16:04:55 +0900

    1002 조현열

--- admin/src/main/java/edu/sm/ChoHyunYeol.java
+++ admin/src/main/java/edu/sm/ChoHyunYeol.java
@@ -1,5 +1,7 @@
 package edu.sm;
 
 public class ChoHyunYeol {
-
+    public static void main(String[] args) {
+        System.out.println("정연균 언제 오노");
+    }
 }
--- service/src/main/java/edu/sm/app/Application.java
+++ service/src/main/java/edu/sm/app/Application.java
@@ -1,5 +1,7 @@
 package edu.sm.app;
 
 public class Application {
-
+    public static void main(String[] args) {
+        System.out.println("언제오냐 임마");
+    }
 }
